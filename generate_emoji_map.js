ea338242e4108c2208da7e710ea2f5eaf4d30ffb
paperluigis <userduck.4@gmail.com>
2023-10-17 18:11:35 +0500

    make emojimap generator more readable

--- generate_emoji_map.js
+++ generate_emoji_map.js
@@ -1,5 +1,11 @@
-const data = require("emojibase-data/en/shortcodes/joypixels.json");
+const data = require("emojibase-data/en/shortcodes/github.json");
 const { writeFileSync } = require("fs");
 // let map=Object.fromEntries(Object.entries(data).map(([j,n])=>[n,String.fromCharPoint(...j.split("-").map(e=>parseInt(e,16)]))]));
-let map = Object.fromEntries(Object.entries(data).map(([j,n])=>[n,String.fromCodePoint(...j.split("-").map(e=>parseInt(e,16)))]))
+let map = Object.fromEntries(Object.entries(data)
+	.map(([j,n])=>[
+		typeof n=="string"?n:n[0],
+		String.fromCodePoint(...j.split("-").map(e=>parseInt(e,16)))
+	])
+)
+
 writeFileSync(__dirname + "/www/emojimap.json", JSON.stringify(map));
